e619995e37abc0b77b2a5cf39cd9c7fc775cd457
Jared Siemen <siemenjm@gmail.com>
2022-09-18 20:50:22 -0400

    Display matchup summary on MatchupTab

--- src/components/Pages.jsx
+++ src/components/Pages.jsx
@@ -7,8 +7,22 @@ import { Routes, Route } from "react-router";
 
 export default function Pages(props) {
     const { BASE_URL, user, league } = useContext(Context);
+    const [users, setUsers] = useState(null);
     const [rosters, setRosters] = useState(null);
     const [matchups, setMatchups] = useState(null);
+
+    async function getUsers() {
+        const URL = `${BASE_URL}league/${league.league_id}/users`;
+        
+        try {
+            const response = await fetch(URL);
+            const allUsers = await response.json();
+
+            setUsers(allUsers);
+        } catch(err) {
+            console.log(err);
+        }
+    }
 
     async function getRosters() {
         const URL = `${BASE_URL}league/${league.league_id}/rosters`;
@@ -37,38 +51,89 @@ export default function Pages(props) {
     }
 
     useEffect(() => {
+        getUsers();
         getRosters();
         getMatchups();
     }, []);
 
+    console.log('Users: ', users);
     console.log('Rosters: ', rosters);
     console.log('Matchups: ', matchups);
 
-    if (!rosters || !matchups) {
+    if (!users || !rosters || !matchups) {
         return <h2>Loading data...</h2>
     }
 
-    const userRoster = rosters.filter((roster) => {
-        return roster.owner_id === user.user_id;
-    });
+    // const userRoster = rosters.filter((roster) => {
+    //     return roster.owner_id === user.user_id;
+    // })[0];
 
-    const userMatchup = matchups.filter((matchup) => {
-        return userRoster[0].roster_id === matchup.roster_id;
-    });
+    // const userMatchup = matchups.filter((matchup) => {
+    //     return userRoster.roster_id === matchup.roster_id;
+    // })[0];
 
-    const opponentMatchup = matchups.filter((matchup) => {
-        return userMatchup[0].matchup_id === matchup.matchup_id && userRoster[0].roster_id !== matchup.roster_id;
-    });
+    // const opponentMatchup = matchups.filter((matchup) => {
+    //     return userMatchup.matchup_id === matchup.matchup_id && userRoster.roster_id !== matchup.roster_id;
+    // })[0];
 
-    console.log('userRoster: ', userRoster[0]);
-    console.log('userMatchup: ', userMatchup[0]);
-    console.log('opponentMatchup: ', opponentMatchup[0]);
+    // get user info from users by user_id
+    function getUserInfo(userId) {
+        const specificUser = users.filter((user) => {
+            return user.user_id === userId;
+        });
+
+        return specificUser[0];
+    }
+
+    // get user roster from rosters by owner_id(user_id)
+    function getUserRoster(userId) {
+        return rosters.filter((roster) => {
+            return roster.owner_id === userId;
+        })[0];
+    }
+
+    // get user matchup from matchups by roster_id
+    function getUserMatchup(userId) {
+        const rosterId = getUserRoster(userId).roster_id;
+        return matchups.filter((matchup) => {
+            return matchup.roster_id === rosterId;
+        })[0];
+    }
+
+    // get user's opponents matchup
+    function getUserOpponentMatchup(userId) {
+        const rosterId = getUserRoster(userId).roster_id;
+        const matchupId = getUserMatchup(userId).matchup_id;
+        return matchups.filter((matchup) => {
+            return matchup.matchup_id === matchupId && matchup.roster_id !== rosterId;
+        })[0];
+    }
+
+    // get user's opponent's roster
+    function getUserOpponentRoster(userId) {
+        const userOpponentMatchup = getUserOpponentMatchup(userId);
+        return rosters.filter((roster) => {
+            return roster.roster_id === userOpponentMatchup.roster_id;
+        })[0];
+    }
+
+    // get user's opponent's id
+
+    function getUserOpponentId(userId) {
+        const userOpponentRoster = getUserOpponentRoster(userId);
+        return userOpponentRoster.owner_id;
+    }
+
+    console.log('userRoster: ', getUserRoster(user.user_id));
+    console.log('userMatchup: ', getUserMatchup(user.user_id));
+    console.log('userOpponentMatchup: ', getUserOpponentMatchup(user.user_id));
+    console.log('userOpponentRoster: ', getUserOpponentRoster(user.user_id));
 
     return (
         <>
             <Routes>
-                <Route path={`/matchup/`} element={<MatchupTab userMatchup={userMatchup[0]} opponentMatchup={opponentMatchup[0]} />} />
-                <Route path={`/team/`} element={<TeamTab userMatchup={userMatchup[0]} />} />
+                <Route path={`/matchup/`} element={<MatchupTab userId={user.user_id} getUserInfo={getUserInfo} getUserOpponentId={getUserOpponentId} getUserMatchup={getUserMatchup} />} />
+                {/* <Route path={`/team/`} element={<TeamTab userMatchup={userMatchup[0]} />} /> */}
                 <Route path={`/league/`} element={<LeagueTab />} />
             </Routes>
         </>
--- src/pages/MatchupTab.jsx
+++ src/pages/MatchupTab.jsx
@@ -1,14 +1,27 @@
-export default function MatchupTab({ userMatchup, opponentMatchup }) {
+export default function MatchupTab({ userId, getUserInfo, getUserOpponentId, getUserMatchup }) {
+    const currentUser = getUserInfo(userId);
+    const currentOpponentId = getUserOpponentId(userId);
+    const currentOpponent = getUserInfo(currentOpponentId);
+    const userMatchup = getUserMatchup(userId);
+    const opponentMatchup = getUserMatchup(currentOpponentId);
+
     return (
         <div className="matchup-tab">
-            <div className="user-stats">
-                <h2>user roster_id: {userMatchup.roster_id}</h2>
-                <h2>user points: {userMatchup.points}</h2>
+            <div className="matchup-summary">
+                <div className="user-matchup-summary">
+                    <img src={`https://sleepercdn.com/avatars/thumbs/${currentUser.avatar}`} alt={`${currentUser.display_name}'s avatar`} />
+                    <h2 className="username">@{currentUser.display_name}</h2>
+                    <p className="team-name">{currentUser.metadata.team_name ? currentUser.metadata.team_name : currentUser.display_name}</p>
+                    <p className="current-points">{userMatchup.points}</p>
+                </div>
+                <div className="opponent-matchup-summary">
+                    <img src={`https://sleepercdn.com/avatars/thumbs/${currentOpponent.avatar}`} alt={`${currentOpponent.display_name}'s avatar`} />
+                    <h2 className="username">@{currentOpponent.display_name}</h2>
+                    <p className="team-name">{currentOpponent.metadata.team_name ? currentOpponent.metadata.team_name : currentOpponent.display_name}</p>
+                    <p className="current-points">{opponentMatchup.points}</p>
+                </div>
             </div>
-            <div className="opponent-stats">
-                <h2>opponent roster_id: {opponentMatchup.roster_id}</h2>
-                <h2>opponent points: {opponentMatchup.points}</h2>
-            </div>
+            
         </div>
     );
 }
