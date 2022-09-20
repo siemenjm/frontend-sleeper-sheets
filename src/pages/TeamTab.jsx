d690b791c16786542460a92a765b95972b009173
Jared Siemen <siemenjm@gmail.com>
2022-09-20 14:32:46 -0400

    Add current user info to top of team tab

--- src/pages/TeamTab.jsx
+++ src/pages/TeamTab.jsx
@@ -5,12 +5,20 @@ import UserBenchList from "components/UserBenchList";
 import UserIRList from "components/UserIRList";
 import UserStarterList from "components/UserStarterList";
 
-export default function TeamTab({ userId, getUserRoster, getUserMatchup }) {
+export default function TeamTab({ userId, getUserInfo, getUserRoster, getUserMatchup }) {
+    const currentUser = getUserInfo(userId); 
     const currentRoster = getUserRoster(userId);
     const currentLineup = getUserMatchup(userId);
 
     return (
         <div className="team-tab tab">
+            <div className="team-tab-user-info">
+                <img src={`https://sleepercdn.com/avatars/thumbs/${currentUser.avatar}`} alt={`${currentUser.display_name}'s avatar`} />
+                <div className="name-info">
+                    <h2 className="username">@{currentUser.display_name}</h2>
+                    <p className="team-name">Team {currentUser.metadata.team_name ? currentUser.metadata.team_name : currentUser.display_name}</p>
+                </div>
+            </div>
             <div className="user-starters">
                 <h2>Starters</h2>
                 <div className="starter-containers">
