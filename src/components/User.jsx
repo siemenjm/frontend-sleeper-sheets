90546fe3bf2a7750a9ec81f44205eba0fc2b3b68
Jared Siemen <siemenjm@gmail.com>
2022-09-20 20:43:13 -0400

    Finish styling sidebar

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,3 +1,4 @@
+import Avatar from "./Avatar";
 import UserLeagues from "./UserLeagues";
 
 export default function User({user}) {    
@@ -13,9 +14,11 @@ export default function User({user}) {
         return (
             <>
                 <div className="user-info">
-                    <h3>Current User</h3>
-                    <p className="user-avatar">Avatar: {user.avatar}</p>
-                    <p className="username">Username: {user.username}</p>
+                    <h2>Current User</h2>
+                    <div>
+                        <Avatar avatar={user.avatar} type='user' />
+                        <p className="username">{user.username}</p>
+                    </div>
                 </div>
                 <div className="league-info">
                     <h2>Current Leagues</h2>
