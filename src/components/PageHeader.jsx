702099bbc0877c31acf8afbc26c266e8d32397ab
Jared Siemen <siemenjm@gmail.com>
2022-09-22 11:44:41 -0400

    Style tab links when active. Cant get links to active style when switching leagues until after clicking on them

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -2,12 +2,32 @@ import { Link, useParams } from "react-router-dom";
 
 export default function PageHeader(props) {
     const { userId, leagueId } = useParams();
+
+    const tabs = [
+        'Matchup',
+        'Team',
+        'League'
+    ];
+
+    const currentPath = window.location.pathname;
+    const splitPath = currentPath.split('/');
+    const currentTab = splitPath[splitPath.length - 2];
+
+    const displayedLinks = tabs.map((tab) => {
+        const path = tab.toLowerCase();
+
+        let cssClass;
+        if (path === currentTab) {
+            cssClass = 'active-tab';
+        }
+
+        return <Link to={`/user/${userId}/league/${leagueId}/${path}/`} className={cssClass} key={`${leagueId}/${path}`}>{tab}</Link>;
+    });
+    
     return (
         <>
             <ul className="page-header">
-                <Link to={`/user/${userId}/league/${leagueId}/matchup/`}>Matchup</Link>
-                <Link to={`/user/${userId}/league/${leagueId}/team/`}>Team</Link>
-                <Link to={`/user/${userId}/league/${leagueId}/league/`}>League</Link>
+                {displayedLinks}
             </ul>
         </>
     );
